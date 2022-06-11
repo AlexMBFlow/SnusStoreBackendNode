89a741cee5c24bf8359c7ee9eb4107cfd0c0c958
hit0tsu__ <Maltzev.alexandr@gmail.com>
2022-06-12 00:05:20 +0300

    добавил отправку ответа на полученный запрос с фронта

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,10 +3,9 @@ import Order from "../models/orderModel.js";
 class OrderController {
     Order(req, res, next) {
         const parce = JSON.parse(req.body.data)
-        parce.basket.forEach( el => {
+        parce.basket.forEach(el => {
             delete el._id
         })
-
         new Order({
             order: {
                 basket: parce.basket,
@@ -15,7 +14,7 @@ class OrderController {
         }).save(err => {
             if (err) throw err
         })
-        res.status(200)
+        res.status(200).send("OK")
     }
 }
 export default new OrderController()
